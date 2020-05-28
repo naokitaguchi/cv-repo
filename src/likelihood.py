77e8213ca70b0fa2ceddd21328cad9bab2c8c172
naokitaguchi <mf20047@shibaura-it.ac.jp>
2020-05-28 23:35:02 +0900

    add source file

---
+++ src/likelihood.py
@@ -0,0 +1,13 @@
+
+
+import numpy as np
+
+def likelihood(img, px, py, obj_value, Np, sigma2 = 0.01):
+    likelihood_arr = []
+    for i in range(Np):
+        b = (img[py[i],px[i]][0] - obj_value[0]) / 255.0
+        g = (img[py[i],px[i]][1] - obj_value[1]) / 255.0
+        r = (img[py[i],px[i]][2] - obj_value[2]) / 255.0
+        likelihood = np.exp(-(r ** 2 + g ** 2 + b ** 2 ) / (2 * sigma2))
+        likelihood_arr.append(likelihood)
+    return likelihood_arr
